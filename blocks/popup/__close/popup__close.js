b9982c40b594a932585b371d3e69b9ac9aa82d2f
Georgiy Shatalov <gashatalov@yandex.ru>
2021-04-15 11:27:24 +0300

    fillform function added

--- blocks/popup/__close/popup__close.js
+++ blocks/popup/__close/popup__close.js
@@ -1,4 +1,8 @@
+let closeButton = document.querySelector('.popup__close');
+
 function closePopup() {
   let popupWindow = document.querySelector('.popup').classList;
   popupWindow.remove('popup_opened');
 }
+
+closeButton.addEventListener('click', closePopup);
--- blocks/profile/__edit-button/profile__edit-button.js
+++ blocks/profile/__edit-button/profile__edit-button.js
@@ -1,4 +1,25 @@
+/* открытие попапа */
+let editButton = document.querySelector('.profile__edit-button');
+
 function openPopup() {
   let popupWindow = document.querySelector('.popup').classList;
   popupWindow.add('popup_opened');
 }
+
+/* вставка данных пользователя в поля формы */
+
+let userName = document.querySelector('.profile__name');
+let userWork = document.querySelector('.profile__work');
+
+let userFormName = document.querySelectorAll('.popup__input-text')[0];
+let userFormWork = document.querySelectorAll('.popup__input-text')[1];
+
+
+function fillForm() {
+  userFormName.setAttribute('value', userName.textContent);
+  userFormWork.setAttribute('value', userWork.textContent);
+}
+
+/* ждем клик по кнопке edit */
+editButton.addEventListener('click', openPopup);
+editButton.addEventListener('click', fillForm);
